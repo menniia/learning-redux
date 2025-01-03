6730c87f03ca2aae4c2a2039ae2e624d745e731d
menniia <mennia17@gmail.com>
2025-01-03 23:02:51 +0000

    updated code

--- redux-news-reader/src/main.jsx
+++ redux-news-reader/src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from '../app/App'
+import { worker } from '../mocks/browser';
+import { StrictMode } from 'react';
+import { Provider } from 'react-redux';
+import store from '../app/store';
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'));
+worker.start();
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider store={store} >
+      <App />
+    </Provider>
+  </StrictMode>
 )
